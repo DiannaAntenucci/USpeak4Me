ab374154c1ece3eb31e1f8005b19593b40d54d9e
Dianna Antenucci <diannarose9@yahoo.com>
2024-07-16 19:57:19 +0900

    change routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,29 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "/jobs", to: "jobs#index"
-  get "/jobs/:id", to: "jobs#show"
-  get "/users/:id", to: "users#show"
-  patch "/users/:id", to: "users#update"
-  get "/jobs/:id/", to: "jobs#show"
-  post "/jobs/:job_id/bookings", to: "bookings#create"
-  get "/bookings", to: "bookings#index"
-  patch "/bookings/:id", to: "bookings#update"
-  post "/users/:id", to: "users#show"
-  get "/jobs/new", to: "jobs#new"
-  post "/jobs", to: "jobs#create"
-  patch "/bookings/:id", to: "bookings#update"
-  get "interpreter/bookings", to: "interpreter::bookings#index"
+  # get "up" => "rails/health#show", as: :rails_health_check
+  # get "/jobs", to: "jobs#index"
+  # get "/jobs/:id", to: "jobs#show"
+  # get "/users/:id", to: "users#show"
+  # patch "/users/:id", to: "users#update"
+  # get "/jobs/:id/", to: "jobs#show"
+  # post "/jobs/:job_id/bookings", to: "bookings#create"
+  # get "/bookings", to: "bookings#index"
+  # patch "/bookings/:id", to: "bookings#update"
+  # post "/users/:id", to: "users#show"
+  # get "/jobs/new", to: "jobs#new"
+  # post "/jobs", to: "jobs#create"
+  # patch "/bookings/:id", to: "bookings#update"
+  # get "interpreter/bookings", to: "interpreter::bookings#index"
 
+  resources :jobs, only: [:index, :show, :new, :create] do
+    resources :bookings, only [:new, :create]
+  end
+
+  resources :bookings, only: [:index, :update]
+  namespace :translator do
+    resources :bookings, only: :index
+    # equivalent to => get 'translator/bookings', to: 'translator/bookings#index'
+  end
   # Defines the root path route ("/")
 end
