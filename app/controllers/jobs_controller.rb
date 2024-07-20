ec8c8d46a8a47328e1a77d2f87ceb6827b87fbae
Dianna Antenucci <diannarose9@yahoo.com>
2024-07-20 12:30:04 +0900

    can create a new job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,14 +1,24 @@
 class JobsController < ApplicationController
-    def new
-        @job = Job.new
-    end
-    
-    def create
-        @job = Job.new(job_params)
-          if @job.save
-            redirect_to job_path(@job)
-          else
-            render :new, status: :unprocessable_entity
-          end
+  def new
+    @job = Job.new
+  end
+  
+  def create
+    @job = Job.new(job_params)
+    @user = current_user
+    # @user.id = @job.user_id
+    @job.user_id = @user.id
+      if @job.save
+        redirect_to job_path(@job)
+      else
+        render :new, status: :unprocessable_entity
       end
+  end
+
+  private
+  
+  def job_params
+    params.require(:job).permit(:job_category, :description, :price, :urgency)
+  end
+
 end
