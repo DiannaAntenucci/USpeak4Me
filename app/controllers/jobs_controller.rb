5bf1757295a8126ce04a47788581434f44c2d0ac
Senie Calalang <senie.c88@gmail.com>
2024-07-20 13:17:37 +0900

    added category to job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,18 +1,21 @@
 class JobsController < ApplicationController
     def new
-        @job = Job.new
+      @job = Job.new
+      @category = ['Hospital', 'City Hall', 'Dentist', 'General Doctor Visit', 'Apartment Search']
+      @urgency = 0
     end
     
     def create
-        @job = Job.new(job_params)
-          if @job.save
-            redirect_to job_path(@job)
-          else
-            render :new, status: :unprocessable_entity
-          end
+      @job = Job.new(job_params)
+        if @job.save
+          redirect_to job_path(@job)
+        else
+          render :new, status: :unprocessable_entity
+        end
       end
 
     def show
       @job = Job.find(params[:id])
     end
 end
+
