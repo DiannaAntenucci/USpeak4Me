7e89fdfaf458faa33f7ff83391a9f121d040b423
Xinyi Wang <rena.wxy@gmail.com>
2024-07-20 14:12:34 +0900

    fix job controller style

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,33 +1,31 @@
 class JobsController < ApplicationController
-
   def index
     @jobs = Job.all
   end
-  
+
   def new
     @job = Job.new
   end
-  
-   def show
+
+  def show
     @job = Job.find(params[:id])
   end
-  
+
   def create
     @job = Job.new(job_params)
     @user = current_user
     # @user.id = @job.user_id
     @job.user_id = @user.id
-      if @job.save
-        redirect_to job_path(@job)
-      else
-        render :new, status: :unprocessable_entity
-      end
+    if @job.save
+      redirect_to job_path(@job)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
-  
+
   def job_params
     params.require(:job).permit(:job_category, :description, :price, :urgency)
   end
-
 end
