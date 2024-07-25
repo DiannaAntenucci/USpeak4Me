43af43311a6f83d0de2b4b6b8fc210f41b539d6a
Senie Calalang <senie.c88@gmail.com>
2024-07-25 19:54:28 +0900

    fixed controller booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,29 @@
 class BookingsController < ApplicationController
-    def index
-        @bookings = Booking.all
+
+    def new
+        @booking = Booking.new
+        @job = Job.find(params[:job_id])
+    end
+
+    def create
+        @job = Job.find(params[:job_id])
+        @booking = Booking.new()
+        @booking.user_id = current_user
+        @booking.job = @job
+        @booking.status = "accepted"
+        @booking.create(booking_params)
+        raise
+        
+        if @booking.save
+            redirect_to job_path(@booking.job_id)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def booking_params
+        params.require(:booking).permit(:status, :job_id)
     end
 end
