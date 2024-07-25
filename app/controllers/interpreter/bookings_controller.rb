dd4e77328a523ef49f944d993969d8c4e718e0b5
Yoosun Jang <yoosunjang0915@gmail.com>
2024-07-25 19:41:12 +0900

    interpreter bookings controller edit

--- app/controllers/interpreter/bookings_controller.rb
+++ app/controllers/interpreter/bookings_controller.rb
@@ -3,5 +3,20 @@ class Interpreter::BookingsController < ApplicationController
         raise
         @bookings = Booking.all
         #need to grab own bookings
+
+    def update
+        @booking = Booking.find(params[:id])
+        if @booking.update(booking_params)
+        # redirect_to # up to you...
+        else
+        # render # where was the booking update form?
+        end
+    end
+
+private
+    def booking_params
+    # TODO: check your model, might be different than mine
+    params.require(:booking).permit(:status, :start_time, :end_time)
+        end
     end
 end
