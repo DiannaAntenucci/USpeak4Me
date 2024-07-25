36c74b5da2bb17ffc1a8c661f687018ada1412a2
Xinyi Wang <rena.wxy@gmail.com>
2024-07-25 20:18:20 +0900

    creates a confirm popup window

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,30 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+import Swal from "sweetalert2";
+
+const buttons = document.querySelectorAll(".confirm-button");
+const form = document.querySelector("#simple_form");
+
+buttons.forEach(button => {
+  button.addEventListener("click", (event) => {
+    // Do something (callback)
+    // alert("Hello there! This is a popup window.");
+    event.preventDefault();
+    Swal.fire({
+      title: "Do you want to save the changes?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Save",
+      denyButtonText: `Don't save`
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        form.submit();
+        Swal.fire("Saved!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
+  });
+});
